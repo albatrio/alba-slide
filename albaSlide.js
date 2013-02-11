c01057d18a5e861858413541c4260d8e3e431d2a
albatrio <ajshortt@albatrio.co.uk>
2013-02-11 12:25:07 -0800

    Create albaSlide.js

---
+++ albaSlide.js
@@ -0,0 +1,28 @@
+$('#s1').cycle({ 
+    fx: 'scrollDown' , 
+  speed:  1000,
+	timeout:  5000,
+ easing: 'easeOutBounce', 
+    delay:  -2000 
+});
+$('#s2').cycle({ 
+    fx: 'scrollDown' , 
+	speed:  1000,
+	timeout:  5000,
+ easing: 'easeOutBounce', 
+    delay:  -2000 
+});
+$('#s3').cycle({ 
+    fx: 'scrollDown' , 
+	speed:  1000,
+	timeout:  5000,
+ easing: 'easeOutBounce', 
+    delay:  -2000 
+});
+$('#s4').cycle({ 
+    fx: 'scrollDown' , 
+	speed:  1000,
+	timeout:  5000,
+ easing: 'easeOutBounce', 
+    delay:  -2000 
+});
